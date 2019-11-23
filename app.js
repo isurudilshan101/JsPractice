bad3885a9b224676c6fe14e8288363e1c213df00
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-11-24 00:02:20 +0530

    comments

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
-let val;
-val=document.all[4];
-val=document.all.length;
-val=document.domain;
-val=document.URL;
-val=document.characterSet;
-val=document.forms;
-val=document.forms[0];
-val=document.forms[0].id;
-val=document.forms[0].method;
-val=document.forms[0].action;
+// let val;
+// val=document.all[4];
+// val=document.all.length;
+// val=document.domain;
+// val=document.URL;
+// val=document.characterSet;
+// val=document.forms;
+// val=document.forms[0];
+// val=document.forms[0].id;
+// val=document.forms[0].method;
+// val=document.forms[0].action;
 
-val=document.links;
-val=document.links[0];
-val=document.links[0].id;
-val=document.links[0].className;
+// val=document.links;
+// val=document.links[0];
+// val=document.links[0].id;
+// val=document.links[0].className;
 
-val=document.images;
+// val=document.images;
 
-val=document.scripts;
-val=document.scripts[2].getAttribute('src');
+// val=document.scripts;
+// val=document.scripts[2].getAttribute('src');
 
-let scripts=document.scripts;
+// // let scripts=document.scripts;
 
-let scriptsArr=Array.from(scripts);
-scriptsArr.forEach(function(script)
-{
-    console.log(script);
-});
-
-console.log(val)
+// // let scriptsArr=Array.from(scripts);
+// // scriptsArr.forEach(function(script)
+// // {
+// //     console.log(script.getAttribute('src'));
+// // });
 
 
- 
+// console.log(val)
 
- 
+
+ //selection
+ //document.getelementById()
+
+//  console.log(document.getElementById('task-title'));
+
+//  //get thigs from the element
+
+//  console.log(document.getElementById('task-title').id);
+
+//  //changing Styling
+
+//  const taskTitle=document.getElementById('task-title');
+
+// document.getElementById('task-title').style.background='#333';
+// document.getElementById('task-title').style.color='#fff';
+//  document.getElementById('task-title').style.padding='5px';
+
+//  //change the content
+//  document.getElementById('task-title').textContent='Task List';
+//  document.getElementById('task-title').innerText='My Tasks';
+//  taskTitle.innerHTML='<span style="color:blue">Task List</span>';
+
+

e679337356bdd3433145e2b524a6d11467fa96b6
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-11-23 22:04:26 +0530

    comments 2

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 let val;
 val=document.all[4];
+val=document.all.length;
+val=document.domain;
+val=document.URL;
+val=document.characterSet;
+val=document.forms;
+val=document.forms[0];
+val=document.forms[0].id;
+val=document.forms[0].method;
+val=document.forms[0].action;
+
+val=document.links;
+val=document.links[0];
+val=document.links[0].id;
+val=document.links[0].className;
+
+val=document.images;
+
+val=document.scripts;
+val=document.scripts[2].getAttribute('src');
+
+let scripts=document.scripts;
+
+let scriptsArr=Array.from(scripts);
+scriptsArr.forEach(function(script)
+{
+    console.log(script);
+});
+
 console.log(val)
 
 
+ 
 
-
+ 

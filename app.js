44560b4d789179bee65922969b2acd777f0cd86c
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-07-23 21:16:46 +0530

    comment 1

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 alert('Hi welcome to the jsss');
+
+
+const squre=function(x){
+    return x*x;
+}
+
+console.log(squre(8));
+
+function greet(){
+    // return 'firstName' + 'lastName';
+    console.log('isuru');
+}
+
+greet();
